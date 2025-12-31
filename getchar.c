6712d15b7ddbdd20aef2ef9f148aee15e8aeb754
nagarathnaby <nagarathnaby27@gmail.com>
2026-01-01 00:18:13 +0530

    Create getchar.c

---
+++ getchar.c
@@ -0,0 +1,22 @@
+// getchar to return ASCIE code  
+#include <stdio.h>
+
+int main() {
+    
+    printf("Enter the character :\n");
+    int ch = getchar();
+    printf("The entered character is %d \n",ch);
+    return 0;
+}
+
+
+// getchar for returing character
+#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter the character :\n");
+    ch = getchar();
+    printf("The entered character is %c \n",ch);
+    return 0;
+}
